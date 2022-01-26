3ee30c9ba53d501981afe2c15f91b807b841ca74
Ryan Ashby <rjapsu160@gmail.com>
2022-01-25 16:59:28 -0800

    refactor and simplify code

--- js/scripts.js
+++ js/scripts.js
@@ -7,33 +7,33 @@ function Ticket(age, time, movie) {
 
 Ticket.prototype.cost = function() {
   let ticketTotal = 0;
-  let ageIncrement = 0;
-  let timeIncrement = 0;
-  let movieIncrement = 0;
+  // let ageIncrement = 0;
+  // let timeIncrement = 0;
+  // let movieIncrement = 0;
 
   if(this.age === "adult") {
-    ageIncrement += 13;
+    ticketTotal += 13;
   } else if(this.age === "senior") {
-    ageIncrement += 8;
+    ticketTotal += 8;
   } else if(this.age === "minor") {
-    ageIncrement += 10;
+    ticketTotal += 10;
   }
   
   if(this.time === "matinee") {
-    timeIncrement -= 4;
+    ticketTotal -= 4;
   } else if(this.age === "evening") {
-    timeIncrement += 2;
+    ticketTotal += 2;
   }
 
   if(this.movie === "dune") {
-    movieIncrement += 4;
+    ticketTotal += 4;
   } else if(this.movie === "eternals") {
-    movieIncrement += 4;
+    ticketTotal += 4;
   } else if(this.movie === "bond") {
-    movieIncrement += 2;
+    ticketTotal += 2;
   }
 
-  ticketTotal = ageIncrement + timeIncrement + movieIncrement;
+  // ticketTotal = ageIncrement + timeIncrement + movieIncrement;
   return ticketTotal;
 }
 
